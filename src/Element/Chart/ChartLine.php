c88ce58cd819af5096896d79f60f79dc736bbd07
毛利小五郎 <zhuchangsong@kuafu.tech>
2024-08-07 10:57:10 +0800

    优化 设置图标数据，节约传输空间

--- src/Element/Chart/ChartLine.php
+++ src/Element/Chart/ChartLine.php
@@ -19,11 +19,26 @@ class ChartLine extends BaseChart
         ];
     }
 
+    /**
+     * 设置图标数据
+     * @param array $data
+     * @param string $xField
+     * @param string $yField
+     * @return $this
+     */
     public function setData(array $data, string $xField, string $yField): ChartLine
     {
+        $fixedData = [];
+        // 只保留图标所需要的数据，其他数据丢了，节约空间
+        foreach ($data as $v) {
+            $fixedData[] = [
+                $xField => $v[$xField],
+                $yField => $v[$yField],
+            ];
+        }
         $this->fillChartSpec([
             'data'   => [
-                'values' => array_values($data),
+                'values' => $fixedData,
             ],
             'xField' => $xField,
             'yField' => $yField,
